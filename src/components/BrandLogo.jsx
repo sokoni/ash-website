67ebb3dbc17ffe03f622c54dde0f3d85c712d44c
sokoni Babb <so.babb@gmail.com>
2026-08-04 22:49:58 -0500

    Expand logo scale and width on coming-soon branch

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default function BrandLogo({ className = "h-40 sm:h-48 md:h-56 lg:h-64 w-auto", onClick }) {
+export default function BrandLogo({ className = "w-[280px] sm:w-[420px] md:w-[560px] lg:w-[680px] h-auto", onClick }) {
   return (
     <div
       onClick={onClick}
-      className={`inline-flex items-center justify-center cursor-pointer select-none shrink-0 ${onClick ? 'hover:opacity-90' : ''}`}
+      className={`inline-flex items-center justify-center cursor-pointer select-none shrink-0 py-2 ${onClick ? 'hover:opacity-90' : ''}`}
     >
       <img
         src="/blackline-logo.png"
         alt="BlackLine Creative Official Logo"
-        className={`${className} object-contain max-h-[20rem] sm:max-h-[24rem] w-auto`}
+        className={`${className} object-contain scale-[3.5] sm:scale-[4] transform origin-center transition-transform duration-300`}
       />
     </div>
   );
